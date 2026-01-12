f0b462278ad006b3c82ab6c5ccd0dc590cb8d962
Aiden Porter <aporter73@outlook.com>
2026-01-12 10:08:51 +1000

    Apply patch for minecraft plugin manager

--- router/router.go
+++ router/router.go
@@ -88,6 +88,7 @@ func Configure(m *wserver.Manager, client remote.Client) *gin.Engine {
 		files := server.Group("/files")
 		{
 			files.GET("/contents", getServerFileContents)
+			files.GET("/fingerprints", getServerFileFingerprints)
 			files.GET("/list-directory", getServerListDirectory)
 			files.PUT("/rename", putServerRenameFiles)
 			files.POST("/copy", postServerCopyFile)
